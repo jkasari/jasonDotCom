a2b268af45575680107880a733688453c136c754
jkasari <jkasari@outlook.com>
2022-09-26 20:47:28 -0700

    Did a bunch of shit and forgot to document!
    I added a js file that inits the text to the html from another java file
    And I added buttons that take you to the blog

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function initMainText() {
+    document.getElementById("intro_text").innerHTML = mainText.SAMPLE_TEXT;
+    document.getElementById("about_text").innerHTML = mainText.SAMPLE_TEXT;
+    document.getElementById("contact_text").innerHTML = mainText.SAMPLE_TEXT;
+}
+
+initMainText();
