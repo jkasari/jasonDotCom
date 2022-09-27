8ab933f727eef1d91db0eaeb0a5196d3e3103592
jkasari <jkasari@outlook.com>
2022-09-26 21:34:16 -0700

    Portfolio tiles are now loading text and images from a json file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
+let portfolioTiles = Array.from(document.getElementsByClassName("portfolio_tile"));
+
+function initPorfolioTiles() {
+    portfolioTiles.forEach(tile => {
+        tile.innerHTML = blogPost[0].TITLE;
+        tile.style.backgroundImage = "url("+blogPost[0].IMAGE_ONE+")";
+        console.log(blogPost[0].IMAGE_ONE);
+    });
+}
+
 function initMainText() {
     document.getElementById("intro_text").innerHTML = mainText.SAMPLE_TEXT;
     document.getElementById("about_text").innerHTML = mainText.SAMPLE_TEXT;
     document.getElementById("contact_text").innerHTML = mainText.SAMPLE_TEXT;
+    initPorfolioTiles();
 }
 
 initMainText();
