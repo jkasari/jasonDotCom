a8984684f33e567e42460c4952ebecd98aaab88b
jkasari <jkasari@outlook.com>
2022-10-01 21:55:13 -0700

    Ready for content!!

--- js/metaData.js
+++ js/metaData.js
@@ -5,7 +5,7 @@ const mainText = {
     "EMAIL": "email | jkasari@outlook.com",
     "PHONE": "phone | 541-610-5575",
     "GITHUB": "github | jkasari",
-};
+}
 
 const colors = {
     "COLOR_1": "#C7CACC",
@@ -40,7 +40,7 @@ const blogPosts = [
         "TITLE_COLOR": "COLOR_1",
         "TILE_TEXT": "- A small blerb about that thing I did.",
         "TILE_IMAGE": "../img/binary_clock/clock.gif",
-        "HEADER_IMAGE": "../imgtest_background.png",
+        "HEADER_IMAGE": "../img/random/test_image_background.png",
         "CONTENT": {
             "IMAGE_1": "../img/random/test_image_wide.jpg",
             "TITLE_1": "This is a Test Title",
@@ -53,7 +53,7 @@ const blogPosts = [
         "TITLE_COLOR": "COLOR_1",
         "TILE_TEXT": "- A small blerb about that thing I did.",
         "TILE_IMAGE": "../img/working_bike_industry/tire_slide.gif",
-        "HEADER_IMAGE": "../img/random/test_background.png",
+        "HEADER_IMAGE": "../img/random/test_image_background.png",
         "CONTENT": {
             "IMAGE_1": "../img/random/test_image_wide.jpg",
             "TITLE_1": "This is a Test Title",
@@ -66,7 +66,7 @@ const blogPosts = [
         "TITLE_COLOR": "COLOR_1",
         "TILE_TEXT": "- A small blerb about that thing I did.",
         "TILE_IMAGE": "../img/learning_electronics/passing_test.jpg",
-        "HEADER_IMAGE": "../img/marcy/lights_on_good.jpg",
+        "HEADER_IMAGE": "../img/random/test_image_background.png",
         "CONTENT": {
             "IMAGE_1": "../img/random/test_image_wide.jpg",
             "TITLE_1": "This is a Test Title",
@@ -79,24 +79,41 @@ const blogPosts = [
         "TITLE_COLOR": "COLOR_1",
         "TILE_TEXT": "- A small blerb about that thing I did.",
         "TILE_IMAGE": "../img/trail_building_alaska/mini_ex.jpg",
-        "HEADER_IMAGE": "../img/random/test_background.png",
+        "HEADER_IMAGE": "../img/trail_building_alaska/rock_lip.jpg",
         "CONTENT": {
-            "IMAGE_1": "../img/random/test_image_wide.jpg",
+            "IMAGE_1": "../img/trail_building_alaska/orange_mini.jpg",
             "TITLE_1": "This is a Test Title",
             "TEXT_1": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "TEXT_2": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "IMAGE_2": "../img/trail_building_alaska/pump_track.jpg",
+            "IMAGE_3": "../img/trail_building_alaska/rock_garden.jpg",
+            "TEXT_3": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "TITLE_2": "This is a Test Title",
+            "TEXT_4": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "IMAGE_4": "../img/trail_building_alaska/mini_ex.jpg",
+            "TEXT_5": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "IMAGE_5": "../img/trail_building_alaska/bug_net.jpg",
         },
     },
     {
         "ID": "5",
         "TITLE": "Trail Building",
-        "TITLE_COLOR": "COLOR_1",
-        "TILE_TEXT": "- A small blerb about that thing I did.",
+        "TITLE_COLOR": "COLOR_3",
+        "TILE_TEXT": "- Trails don't build themselves.",
         "TILE_IMAGE": "../img/trail_building_washington/moss_remove.jpg",
-        "HEADER_IMAGE": "../img/random/test_background.png",
+        "HEADER_IMAGE": "../img/trail_building_washington/chainsaw_1.jpg",
         "CONTENT": {
-            "IMAGE_1": "../img/random/test_image_wide.jpg",
             "TITLE_1": "This is a Test Title",
             "TEXT_1": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "IMAGE_1": "../img/trail_building_washington/logs.jpg",
+            "TEXT_2": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "TEXT_3": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "IMAGE_2": "../img/trail_building_washington/grimice1.jpg",
+            "TITLE_2": "This is a Test Title",
+            "TEXT_4": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "TITLE_3": "This is a Test Title",
+            "TEXT_5": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "IMAGE_3": "../img/trail_building_washington/drop.JPG",
         },
     },
 ];
